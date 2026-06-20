d23dab2fb880932d2af02ea43081b76e48e0a4c0
harshpa707 <harshpa707@gmail.com>
2026-06-20 15:14:19 +0530

    auth and db sync

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,13 +1,23 @@
 import express from "express";
 import path from "path";
+import cors from "cors";
+import { serve } from "inngest/express";
 import dotenv from "dotenv";
 import { ENV } from "./lib/env.js";
 import { connectDB } from "./lib/db.js";
+import { inngest } from "./lib/inngest.js";
 
 dotenv.config();
 
 const app = express();
 const __dirname = path.resolve();
+
+//middleware
+app.use(express.json());
+//redentials:true meaning?? => server allows a browser to include cookies on request
+app.use(cors({ origin: ENV.CLIENT_URL, credentials: true }));
+
+app.use("/api/inngest", serve({client:inngest , functions}));
 
 app.get("/health", (req, res) => {
   res.status(200).json({ msg: "api is up and running" });
@@ -31,7 +41,7 @@ const startServer = async () => {
       console.log("Server is running on port:", ENV.PORT),
     );
   } catch (error) {
-    console.error("Error Starting the Server",error)
+    console.error("Error Starting the Server", error);
   }
 };
 startServer();
